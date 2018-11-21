9a6ffdb4015f23dea72a6108bc61ec75f1c6f48c
Alexis <lambertalexis2@gmail.com>
2018-11-21 17:10:09 +0100

    Logo

--- apps/front/src/views/Home.vue
+++ apps/front/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <Logo></Logo>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +8,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Logo from '@/svg/Logo.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Logo
   }
 }
 </script>
