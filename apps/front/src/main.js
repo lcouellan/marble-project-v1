3ab55758d0c78bab9360defc4068928df1ebc3fc
Alexis <lambertalexis2@gmail.com>
2018-11-21 16:20:47 +0100

    Replace Vue cli-2 with Vue cli-3

--- apps/front/src/main.js
+++ apps/front/src/main.js
@@ -1,19 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import 'es6-promise/auto'
-import Vuex from 'vuex'
-import './assets/sass/app.scss'
-Vue.use(Vuex)
+import store from './store'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
